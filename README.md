546c9df0d5c2ec8ea7bbd85f76e8b8bc48f39d3d
Fernando Derkoski <brilvio@gmail.com>
2016-09-19 12:59:27 -0300

    added readme and fixed index.js

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to use it
+* You have to make sure you have Node.js installed
+* In the package.json set the name of server
+* By default he will run in the 8080 port, you can set the Envirement var named: MONITORPORT with the new port. 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Lets require/import the HTTP module
 var http = require('http');
 var pjson = require('./package.json');
+var exec = require('child_process').exec;
 
 //Lets define a port we want to listen to
 const PORT = 8080;
@@ -13,21 +14,17 @@ function handleRequest(request, response) {
 
 }
 
-var exec = require('child_process').exec;
-
-
 function isFactorioRunning(cb) {
     var res = {};
     res.server = pjson.serverName;
     var child;
     // executes `pwd`
     exec("ps aux | grep factorio", function (error, stdout, stderr) {
-        sys.print('stdout: ' + stdout);
-        sys.print('stderr: ' + stderr);
         if (error !== null) {
+            res.error = error;
             console.log('exec error: ' + error);
         }
-        res.isRunning = true;
+        res.isRunning = stdout.indexOf('/opt/factorio/') > 0;
         cb(JSON.stringify(res));
     });
 }
@@ -36,7 +33,7 @@ function isFactorioRunning(cb) {
 var server = http.createServer(handleRequest);
 
 //Lets start our server
-server.listen(process.env.PORT || PORT, function () {
+server.listen(process.env.MONITORPORT || PORT, function () {
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", process.env.PORT || PORT);
 });
